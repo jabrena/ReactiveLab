417fbf0bb52379b1ec6142e75e7fcbce8ece8ebf
Juan Antonio Breña Moral <bren@juanantonio.info>
2016-11-01 23:12:23 +0000

    minor change

--- www/Scripts/game1/index.js
+++ www/Scripts/game1/index.js
@@ -66,7 +66,8 @@ $(function() {
 
 		var ifttt = "https://maker.ifttt.com/trigger/winter-games-game1/with/key/dvHJXlCso1laUplLipy7LT";
         //fetch("http://anyorigin.com/go?url=https://maker.ifttt.com/trigger/winter-games-game1/with/key/dvHJXlCso1laUplLipy7LT",
-        fetch("http://whateverorigin.org/get?url=" + encodeURIComponent(ifttt),
+        //fetch("http://whateverorigin.org/get?url=" + encodeURIComponent(ifttt),
+        fetch("https://crossorigin.me/https://maker.ifttt.com/trigger/winter-games-game1/with/key/dvHJXlCso1laUplLipy7LT",
         {
         	method: "POST",
         	mode: 'cors', 
