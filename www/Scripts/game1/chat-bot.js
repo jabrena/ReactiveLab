9f25c46b192830ec42472af9f17d784ba1954c90
Juan Antonio Breña Moral <bren@juanantonio.info>
2016-11-01 20:22:04 +0000

    Minor change

--- www/Scripts/game1/chat-bot.js
+++ www/Scripts/game1/chat-bot.js
@@ -11,16 +11,16 @@ function chatBot() {
         this.input = input.toLowerCase();
         
         if(this.match('(hi|hello|hey|hola|howdy)(\\s|!|\\.|$)'))
-            return "um... hi?";
+            return ["um... hi?"];
         
         if(this.match('what[^ ]* up') || this.match('sup') || this.match('how are you'))
-            return "this github thing is pretty cool, huh?";
+            return ["this github thing is pretty cool, huh?"];
         
         if(this.match('l(ol)+') || this.match('(ha)+(h|$)') || this.match('lmao'))
-            return "what's so funny?";
+            return ["what's so funny?"];
         
         if(this.match('^no+(\\s|!|\\.|$)'))
-            return "don't be such a negative nancy :(";
+            return ["don't be such a negative nancy :("];
         
         if(this.match('(cya|bye|see ya|ttyl|talk to you later)'))
             return ["alright, see you around", "Good luck with the challenge!"];
