8a52478c7fdcfc2ec672f72c1c1eba8a0eb7b694
Juan Antonio Breña Moral <bren@juanantonio.info>
2016-11-02 00:03:13 +0000

    Minor change

--- www/Scripts/game1/chat-bot.js
+++ www/Scripts/game1/chat-bot.js
@@ -42,77 +42,90 @@ function chatBot() {
     
         this.input = input.toLowerCase();
         
-        if(this.match('(hi|hello|hey|hola|howdy)(\\s|!|\\.|$)'))
+        if(this.match('(hi|hello|hey|hola|howdy)(\\s|!|\\.|$)')){
             return [
                 "um... hi?"];
+        }
         
-        if(this.match('(robin)(\\s|!|\\.|$)'))
+        if(this.match('(robin)(\\s|!|\\.|$)')){
             return [
                 "This is my name. Do you need some info about something?"];
+        }
 
-        if(this.match('what[^ ]* up') || this.match('sup') || this.match('how are you'))
+        if(this.match('what[^ ]* up') || this.match('sup') || this.match('how are you')){
             return [
                 "this github thing is pretty cool, huh?"];
+        }
         
-        if(this.match('l(ol)+') || this.match('(ha)+(h|$)') || this.match('lmao'))
+        if(this.match('l(ol)+') || this.match('(ha)+(h|$)') || this.match('lmao')){
             return [
                 "what's so funny?"];
+        }
         
-        if(this.match('^no+(\\s|!|\\.|$)'))
+        if(this.match('^no+(\\s|!|\\.|$)')){
             return [
                 "don't be such a negative nancy :("];
+        }
         
-        if(this.match('(cya|bye|see ya|ttyl|talk to you later)'))
+        if(this.match('(cya|bye|see ya|ttyl|talk to you later)')){
             return [
                 "alright, see you around", 
                 "Good luck with the challenge!"];
+        }
         
-        if(this.match('(dumb|stupid|is that all)'))
+        if(this.match('(dumb|stupid|is that all)')){
             return [
                 "hey i'm just a proof of concept", 
                 "you can make me smarter if you'd like"];
+        }
         
-        if(this.match('(game|game1|challenge|winter game|game)'))
+        if(this.match('(game|game1|challenge|winter game|game)')){
             return [
                 "Nice, we are going to play...",
                 "Do you like UX? Or...", 
                 "Do you like middleware plumbing?"];
+        }
 
-        if(this.match('(1|ux|frontend|front-end|client)'))
+        if(this.match('(1|ux|frontend|front-end|client)')){
             return [
                 "Oki,",
                 "What is your favourite channel technology? (Javascript, IOS or Android)"];
+        }
 
-        if(this.match('(javascript|js)'))
+        if(this.match('(javascript|js)')){
             this.ping("winter-games-game1-ux");
             return [
                 "In the first game, you have to learn to publish a Static WebApp on your favourite Cloud Provider. (AWS, Bluemix, PWS or Google Cloud)",
                 "Get extra points if you read some stuff about reactive extensions for your favourite Javascript library/framework (JQuery, React & Angular 2).",
                 "You have 2 weeks to get your best in this game"];
+        }
 
-        if(this.match('(android|ios|swift|objective c|native)'))
+        if(this.match('(android|ios|swift|objective c|native)')){
             this.ping("winter-games-game1-ux");
             return [
                 "In the first game, you have to learn to publish a Native application on your favourite Cloud Provider. (AWS, Bluemix, PWS or Google Cloud)",
                 "Get extra points if you read some stuff about reactive extensions for your favourite front-end environment.",
                 "You have 2 weeks to get your best in this game"];
+        }
         
-        if(this.match('(2|backend|middleware|spring)'))
+        if(this.match('(2|backend|middleware|spring)')){
             this.ping("winter-games-game1-java");
             return [
                 "Oki,",
                 "In the first game, you have to learn to publish a REST API on your favourite Cloud Provider. (AWS, Bluemix, PWS or Google Cloud)",
                 "Get extra points if you add some feature about Spring Cloud Contract.",
                 "You have 2 weeks to get your best in this game"];
+        }
 
-        if(this.match('(rules)'))
+        if(this.match('(rules)')){
             return [
                 "Store your game on this repo or in your personal repository on Github.",
                 "Try to not use the same PAAS provider.",
                 "With the game deployed, paste your results on issue #4",
                 "Enjoy with the technology and learn"];
+        }
 
-        if(this.match('(extra points)'))
+        if(this.match('(extra points)')){
             return ["You get extra points in the following cases:",
                     "Use Spring Boot 2.0",
                     "Use complex Spring bean configuration on your examples",
@@ -121,12 +134,14 @@ function chatBot() {
                     "Apply cache techiques",
                     "Add HTTP/2 without using Servlet Spec.",
                     "Use Spring Reactive"];
+        }
 
-        if(this.match('(help)'))
+        if(this.match('(help)')){
             return [
                 "Type the right question",
                 "If the problem continues, Create an Issue on Github. ",
                 "My creator will help you."];
+        }
 
         if(this.match('(about)'))
             return ["Created with ❤️  in Milton Keynes, UK"];
